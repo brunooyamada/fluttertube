853743b3afde29380ba8ad27838a9932ce36dfda
Bruno Oyamada <bruno.oyamada@hotmail.com>
2026-03-04 14:33:21 -0300

    Finalizando o Sistema de Buscas

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -20,8 +20,12 @@ class Home extends StatelessWidget {
           IconButton(icon: Icon(Icons.star), onPressed: () {}),
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: () {
-              showSearch(context: context, delegate: DataSearch());
+            onPressed: () async {
+              String? result = await showSearch(
+                context: context,
+                delegate: DataSearch(),
+              );
+              print(result);
             },
           ),
         ],
